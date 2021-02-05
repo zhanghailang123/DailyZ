6e0392f90f919061aea45c9d529bdab74db07183
zhanghailang123 <1187000351@qq.com>
2021-02-05 14:10:47 +0800

    LeetCode 209

--- src/main/java/LeetCode/SlipWindow.java
+++ src/main/java/LeetCode/SlipWindow.java
@@ -1,6 +1,5 @@
 package LeetCode;
 
-import javax.validation.constraints.Min;
 
 /**
  * @description: leetcode上部分关于滑动窗口解法的题目
@@ -19,20 +18,93 @@ public class SlipWindow {
      * 解释：子数组 [4,3] 是该条件下的长度最小的子数组。
      */
     public static int minLenOfArray(int[] array,int s){
-        int re = 0;
+        /**
+         * 自己写的暴力法 有点小问题
+         */
+        int re = 100;
         for (int i = 0; i < array.length; i ++){
-            for (int j = 1; j < array.length; j++){
+            for (int j = i; j < array.length; j++){
                 if (array[i] + array[j] >= s){
                     re = j - i;
-                    return Math.min(re,j-i-1);
+                    return Math.min(re,j-i+1);
                 }
             }
         }
         return re;
     }
 
+    public static int minLenOfArray1(int[] array,int s){
+        /**
+         * 修复后的暴力法
+         */
+        //初始化子数组长度为数组长度+1
+        int result = array.length + 1;
+        for (int i = 0; i < array.length; i ++){
+            int sum = 0;
+            for (int j = i; j < array.length; j++){
+                sum += array[j];
+                if (sum >= s){
+                    //如果当前累加和大于正整数，则更新子数组长度
+                    result = Math.min(result,j - i + 1);
+                }
+            }
+        }
+        //如果最终result的值等于数组长度+1，则表示不存在符合条件的子数组
+        return result == array.length + 1 ? 0 : result;
+    }
+
+    /**
+     * 双指针
+     * @param array
+     * @param s
+     * @return
+     */
+    public static int minLenOfArray2(int[] array,int s){
+        if (array.length == 0){
+            return 0;
+        }
+        int left  = 0;
+        int right = 0;
+        int sum = 0 ;
+        int re = array.length + 1;
+        while (sum < s) {
+            if (right <= array.length){
+                right = right + 1;
+                sum += array[right];
+            }
+        }
+        return 1;
+    }
+
+    public static int minLenOfArray3(int[] array,int s){
+        if (array.length == 0){
+            return 0;
+        }
+        int left = 0;
+        int right = -1;
+        int sum = 0;
+        int re = array.length + 1;
+        while (left < array.length){
+            if (right + 1 < array.length && sum < s){
+                right ++;
+                sum += array[right];
+
+            }else {
+                sum -= array[left];
+                left ++;
+            }
+            if (sum >= s){
+//                re = right - left + 1;
+//                return re == array.length + 1 ? 0 : re;
+                re = Math.min(re,right - left + 1);
+            }
+        }
+        return re == array.length + 1 ? 0 : re;
+    }
     public static void main(String[] args) {
         int[] nums = {2,3,1,2,4,3};
-        System.out.println(minLenOfArray(nums,8));
+        System.out.println(minLenOfArray(nums,6));
+        System.out.println(minLenOfArray1(nums,6));
+        System.out.println(minLenOfArray3(nums,7));
     }
 }
