46410b7fa7134ea798bcd1bd86bb97f0b3547a30
zhanghailang123 <1187000351@qq.com>
2021-02-26 19:23:36 +0800

    主要是练习下分治法

--- src/main/java/LeetCode/LongestComStr.java
+++ src/main/java/LeetCode/LongestComStr.java
@@ -1,5 +1,7 @@
 package LeetCode;
 
+import org.apache.coyote.OutputBuffer;
+
 /**
  * @description: 最长公共前缀
  * https://leetcode-cn.com/problems/longest-common-prefix/solution/zui-chang-gong-gong-qian-zhui-by-leetcode-solution/
@@ -8,6 +10,11 @@ package LeetCode;
  * @date: 2021/2/25 0025 22:57
  */
 public class LongestComStr {
+    /**
+     * 横向比较
+     * @param strs
+     * @return
+     */
     public static String solution(String[] strs){
         if (strs == null || strs.length == 0){
             return "";
@@ -21,7 +28,6 @@ public class LongestComStr {
             }
         }
         return prefix;
-
     }
     public static String longestStr (String str1,String str2){
         int length = Math.min(str1.length(),str2.length());
@@ -30,5 +36,71 @@ public class LongestComStr {
             index ++;
         }
         return str1.substring(0,index);
+    }
+
+
+    /**
+     * 纵向比较
+     * Math.min
+     * Math.max
+     * @param strs
+     */
+
+    public static String solution2(String[] strs){
+        if (strs == null || strs.length == 0){
+            return "";
+        }
+
+//        for (int i = 0; i < strs.length; i++){
+//            for (int j = 0; j < strs[i].length(); j++){
+//
+//            }
+//        }
+        int pLength = strs[0].length();
+        for (int i = 0; i < pLength; i++){
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++){
+//                if (strs[j].charAt(i) != c){
+//                    break;
+//                }
+                if (i == strs[j].length() || strs[j].charAt(i) != c){
+                    return strs[0].substring(0,i);
+                }
+            }
+//            return strs[0].substring(0,i);
+        }
+        return strs[0];
+    }
+
+    /**
+     * 分治法
+     * @param strs
+     * @return
+     */
+    public static String solution3(String[] strs){
+        return "";
+    }
+
+    public String longestCommonPrefix(String[] strs,int start,int end){
+        if (start == end){
+            return strs[start];
+        }
+        return null;
+    }
+
+    public String commonPrefix(String lcpLeft,String lcpRight){
+        int minLength = Math.min(lcpLeft.length(),lcpRight.length());
+        for (int i = 0; i < minLength; i++){
+            if (lcpLeft.charAt(i) != lcpRight.charAt(i)){
+                return lcpLeft.substring(0,i);
+            }
+        }
+        return lcpLeft.substring(0,minLength);
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"abcde","abcdfe","abcrt"};
+        System.out.println(solution(strs));
+        System.out.println(solution2(strs));
     }
 }
