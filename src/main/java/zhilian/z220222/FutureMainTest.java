dd045cfc7f5b126895adae0db82c355867e4d230
zhanghailang <1187000351@qq.com>
2022-02-28 21:36:56 +0800

    FutureTask

--- src/main/java/zhilian/z220222/FutureMainTest.java
+++ src/main/java/zhilian/z220222/FutureMainTest.java
@@ -12,23 +12,34 @@ public class FutureMainTest {
 
     public static void main(String[] args) throws InterruptedException {
         ScoreQueryService service = new ScoreQueryService();
-        ExecutorService executorService = Executors.newFixedThreadPool(5);
-        for (int i = 0; i < 1; i++) {
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+        for (int i = 0; i < 3; i++) {
             executorService.execute(() -> {
                 Integer re = null;
                 try {
                     re = service.query("ZHL");
                 } catch (ExecutionException e) {
                     e.printStackTrace();
-                } catch (InterruptedException e) {
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
                 System.out.println("分数：" + re);
             });
         }
-        TimeUnit.SECONDS.sleep(1);
-        for (Map.Entry<String, Future<Integer>> stringFutureEntry : ScoreQueryService.SCORE_CACHE.entrySet()) {
-            stringFutureEntry.getValue().cancel(true);
-        }
+        Thread.sleep(5000);
+        executorService.execute(() -> {
+            System.out.println("又一次查询开始");
+            try {
+                service.query("ZHL");
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+//        TimeUnit.SECONDS.sleep(1);
+//        for (Map.Entry<String, Future<Integer>> stringFutureEntry : ScoreQueryService.SCORE_CACHE.entrySet()) {
+//            stringFutureEntry.getValue().cancel(true);
+//        }
     }
 }
