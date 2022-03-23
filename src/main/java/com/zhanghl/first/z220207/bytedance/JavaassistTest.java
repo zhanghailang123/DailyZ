33d909f623000c902d3a25884afe7605fe9c32e4
zhanghailang <1187000351@qq.com>
2022-03-23 23:07:46 +0800

    JOL测试

---
+++ src/main/java/com/zhanghl/first/z220207/bytedance/JavaassistTest.java
@@ -0,0 +1,13 @@
+package com.zhanghl.first.z220207.bytedance;
+
+/**
+ * @description: javaassist test
+ * @author: zhanghailang
+ * @date: 2022/3/23 0023 22:37
+ */
+public class JavaassistTest {
+
+    public static void main(String[] args) {
+        Classp
+    }
+}
