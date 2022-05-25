921e96b9390e5aec26cacca888e6f4c8ccbca849
hailang.zhang <hailang.zhang@zhaopin.com.cn>
2022-05-25 09:40:49 +0800

    JOL测试

---
+++ src/main/java/com/zhanghl/first/wuyi/ComletionServiceTest.java
@@ -0,0 +1,25 @@
+package com.zhanghl.first.wuyi;
+
+
+import java.util.HashMap;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @description:
+ * @author: zhanghailang
+ * @date: 2022/5/9 0009 21:18
+ */
+public class ComletionServiceTest {
+    public static void main(String[] args) {
+        public static void main(String[] args) {
+            HashMap map = new HashMap();
+            HashMap mapK = new HashMap();
+            mapK.put("sourceFrom","CLOUDRESUME");
+            mapK.put("onceToken","2088142505313523-2");
+            map.put("kaAppContext",mapK);
+//            System.out.println(Jsonobject.toJSONString(map));
+        }
+    }
+}
