4a5bb153d60398e59ccc2c1a32554ca5e86c0bbf
hailang.zhang <hailang.zhang@zhaopin.com.cn>
2022-06-02 18:19:34 +0800

    test

---
+++ src/main/java/com/zhanghl/first/statemachine/TestStateMachine.java
@@ -0,0 +1,17 @@
+package com.zhanghl.first.statemachine;
+
+import com.alibaba.cola.statemachine.builder.StateMachineBuilder;
+import com.alibaba.cola.statemachine.builder.StateMachineBuilderFactory;
+
+/**
+ * @description: cola 状态机测试使用
+ * @author: zhanghailang
+ * @date: 2022/6/2 17:44
+ */
+public class TestStateMachine {
+
+    public static void main(String[] args) {
+        StateMachineBuilder<Object, Object, Object> builder = StateMachineBuilderFactory.create();
+        builder.internalTransition().within().on().when().perform();
+    }
+}
