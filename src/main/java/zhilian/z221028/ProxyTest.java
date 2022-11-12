a0283afdef71a1d0bbbdf33a56dccd3154b8bcde
hailang.zhang <hailang.zhang@zhaopin.com.cn>
2022-11-12 09:52:36 +0800

    feature:JDK动态代理测试使用

--- src/main/java/zhilian/z221028/ProxyTest.java
+++ src/main/java/zhilian/z221028/ProxyTest.java
@@ -5,6 +5,7 @@ package zhilian.z221028;
  * @since 2022/10/28 21:45
  **/
 public class ProxyTest {
+
     public static void main(String[] args) {
         UserServiceImpl param = new UserServiceImpl();
         UserService service = MyProxyUtil.getProxyByJDK(param);
