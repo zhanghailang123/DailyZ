d02c57633bd414f491a6d86c8b2704bd6e4a47a4
hailang.zhang <hailang.zhang@zhaopin.com.cn>
2023-08-15 16:42:02 +0800

    test for Test

---
+++ src/main/java/zhilian/上上签/TestApi.java
@@ -0,0 +1,23 @@
+package zhilian.上上签;
+
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson.JSONObject;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Date;
+
+/**
+ * @author hailang.zhang
+ * @since 2023-08-10
+ */
+public class TestApi {
+
+    public static void main(String[] args) throws IOException {
+    }
+}
