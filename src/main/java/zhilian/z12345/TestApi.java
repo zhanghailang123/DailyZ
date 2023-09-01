c0eb2fa4a60b36af6d54ec5f665684fb6c070fc7
hailang.zhang <hailang.zhang@zhaopin.com.cn>
2023-09-01 10:15:23 +0800

    test for Test

--- src/main/java/zhilian/z12345/TestApi.java
+++ src/main/java/zhilian/z12345/TestApi.java
@@ -1,6 +1,10 @@
 package zhilian.z12345;
 
 import java.io.IOException;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author hailang.zhang
@@ -9,5 +13,13 @@ import java.io.IOException;
 public class TestApi {
 
     public static void main(String[] args) throws IOException {
+        Executors.newFixedThreadPool(5);
+        Executors.newSingleThreadExecutor();
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                5,
+                5,
+                5,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(100));
     }
 }
