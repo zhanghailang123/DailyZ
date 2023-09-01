40cd6d43bc6ffff640c2ab97390e3852807a0798
hailang.zhang <hailang.zhang@zhaopin.com.cn>
2023-09-01 18:35:42 +0800

    test for Test

---
+++ src/main/java/zhilian/z230824/HowToStopThreadPool.java
@@ -0,0 +1,13 @@
+package zhilian.z230824;
+
+/**
+ * 怎么去终止一个线程池
+ * @author hailang.zhang
+ * @since 2023-09-01
+ */
+public class HowToStopThreadPool {
+
+    public static void main(String[] args) {
+
+    }
+}
