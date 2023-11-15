a4fa0bedee334a07f5a07dfeaec050737e5e8fa3
hailang.zhang <hailang.zhang@zhaopin.com.cn>
2023-11-15 13:58:40 +0800

    状态机

--- src/main/java/zhilian/z231018/statemc/Action.java
+++ src/main/java/zhilian/z231018/statemc/Action.java
@@ -4,5 +4,7 @@ package zhilian.z231018.statemc;
  * @author hailang.zhang
  * @since 2023-10-31
  */
-public interface Action {
+public interface Action <S, E, C>{
+
+    void execute(S from, S to, E event, C context);
 }
--- src/main/java/zhilian/z231018/statemc/Condition.java
+++ src/main/java/zhilian/z231018/statemc/Condition.java
@@ -4,5 +4,11 @@ package zhilian.z231018.statemc;
  * @author hailang.zhang
  * @since 2023-10-31
  */
-public interface Condition {
+public interface Condition<C> {
+
+    boolean isSatisfied(C context);
+
+    default String name() {
+        return this.getClass().getSimpleName();
+    }
 }
